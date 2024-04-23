c9693a632623e6ab90c72a59cfbbb70830d2ddeb
linh-nv <nvlinh.rin@gmail.com>
2024-04-23 14:06:09 +0700

    facebook-menu(90%), CRUD category

--- html-css-js/book-manager/public/templates/assets/js/main.js
+++ html-css-js/book-manager/public/templates/assets/js/main.js
@@ -1,22 +1,17 @@
-// Lấy phần tử menu click và sidebar menu
 const menuClick = document.getElementById('menu-click');
 const sidebarMenu = document.querySelector('.sidebar-menu');
 const logo = document.querySelector('.logo');
 const topBar = document.querySelector('.top-bar');
 const sidebarItems = document.querySelectorAll('.sidebar-menu .product-text span');
 
-// Thêm sự kiện click cho menu click
 menuClick.addEventListener('click', function() {
-    // Kiểm tra width hiện tại của sidebar menu
     const currentWidth = sidebarMenu.offsetWidth;
 
-    // Kiểm tra nếu width hiện tại là 240px, thì đặt width mới là 100px, ngược lại đặt width mới là 240px
     if (currentWidth === 240) {
         sidebarMenu.style.width = '100px';
         logo.style.opacity = 0;
         topBar.style.width = 'calc(100vw - 100px)';
         
-        // Với mỗi span trong sidebar items, đặt opacity là 0 và pointer-events là none
         sidebarItems.forEach(function(span) {
             span.style.opacity = '0';
             span.style.pointerEvents = 'none';
@@ -26,7 +21,6 @@ menuClick.addEventListener('click', function() {
         logo.style.opacity = 1;
         topBar.style.width = 'calc(100vw - 240px)';
         
-        // Với mỗi span trong sidebar items, đặt lại opacity và pointer-events về giá trị mặc định
         sidebarItems.forEach(function(span) {
             span.style.opacity = '';
             span.style.pointerEvents = '';
@@ -36,42 +30,112 @@ menuClick.addEventListener('click', function() {
 
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    showCategory();
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var selectElement = document.getElementById('genre');
+    data.forEach(function(item) {
+        var option = document.createElement('option');
+        option.value = item.id; 
+        option.textContent = item.title; 
+        selectElement.appendChild(option); 
+    });
+    // showCategory();
+});
+var data = [
+    {
+        "id": 1,
+        "title": "Fiction"
+    },
+    {
+        "id": 2,
+        "title": "Classic"
+    },
+    {
+        "id": 3,
+        "title": "Fantasy"
+    }
+]
+function submitCategory(){
+    var category_id = data[data.length - 1].id + 1;
+    var category_title = document.getElementById('category-title').value
+    var category_id = document.getElementById('category-id').value
 
+    var item = {
+        id: category_id,
+        title: category_title
+    }
 
-let formDataArray = [];
+    let index = data.findIndex((c)=>c.id == item.id)
+    console.log(index);
+    if(index >= 0){
+        data.splice(index, 1, item)
+    }else{   
+        data.push(item)
+    }
 
-document.getElementById('upload-photo').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const imageUrl = URL.createObjectURL(file);
-    console.log(imageUrl);
-    formDataArray.push(imageUrl);
+    showCategory()
+    clearCategory()
+}
+
+function showCategory(){
+    table = `
+    <tr class="table-title">
+        <th>ID</th>
+        <th>Category</th>
+        <th>Action</th>
+    </tr>
+    `
+    for(let i=0; i<data.length; i++){
+        table += `
+        <tr>
+            <td>${data[i].id}</td>
+            <td>${data[i].title}</td>
+            <td>
+                <div class="action">
+                    <div class="edit" onclick="editCategory(${data[i].id})">
+                        <img src="./assets/icons/pencil-write.svg" alt="">
+                    </div>
+                    <div class="delete" onclick="deleteCategory(${data[i].id})">
+                        <img src="./assets/icons/bin.svg" alt="">
+                    </div>
+                </div>
+            </td>
+        </tr>
+        `
+    }
+    document.getElementById('table').innerHTML = table;
+}
+
+function clearCategory(){
+    document.getElementById('category-title').value = ""
+    document.getElementById('category-id').value = ""
+}
+function deleteCategory(id){
+    for(let i=0; i<data.length; i++){
+        if(data[i].id == id){
+            data.splice(i,1)
+
+            showCategory()
+        }
+    }
+}
+function editCategory(id){
+    for(let i=0; i<data.length; i++){
+        if(data[i].id == id){
+            document.getElementById('category-id').value = data[i].id
+            document.getElementById('category-title').value = data[i].title
+        }
+    }
+}
+
+const uploadPhotoInput = document.getElementById('upload-photo');
+uploadPhotoInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  const imagePath = file.path;
+  console.log(imagePath);
 });
 
-
-document.querySelector('.form-box').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const firstName = document.getElementById('fist-name').value;
-    const lastName = document.getElementById('last-name').value;
-    const email = document.getElementById('your-email').value;
-    const phoneNumber = document.getElementById('phone-number').value;
-    const dateOfBirth = document.getElementById('date-of-birth').value;
-    const genre = document.getElementById('genre').value;
-
-    const formData = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        phoneNumber: phoneNumber,
-        dateOfBirth: dateOfBirth,
-        genre: genre
-    };
-
-    // Thêm đối tượng formData vào formDataArray
-    formDataArray = [...formDataArray, formData];
-
-    // Log kết quả
-    // console.log(formDataArray);
-});
 
 
