6ded3abceb0a956df44e375ab8e599b025040980
linh-nv <nvlinh.rin@gmail.com>
2024-05-28 12:46:20 +0700

    complete lendTicket pages

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../pages/LoginPage.vue";
 import Home from "../pages/HomePage.vue";
-
+import LendTicket from "../pages/LendTicketPage.vue";
+import LendTicketForm from "../components/LendTicket/LendTicketForm.vue";
+import LendTicketList from "../components/LendTicket/LendTicketList.vue";
 import { useUserStore } from "../stores/userStore";
 import { useActiveRouteStore } from "../stores/activeRouteStore";
 
@@ -22,6 +24,29 @@ const routes = [
     path: "/home",
     name: "home",
     component: Home,
+  },
+  {
+    path: "/lend-ticket",
+    name: "lend-ticket",
+    component: LendTicket,
+    redirect: "/lend-ticket/list",
+    children: [
+      {
+        path: "list",
+        name: "lend-ticket-list",
+        component: LendTicketList,
+      },
+      {
+        path: "form",
+        name: "lend-ticket-form",
+        component: LendTicketForm,
+      },
+      {
+        path: "form/:id",
+        name: "lend-ticket-form-edit",
+        component: LendTicketForm,
+      },
+    ],
   },
 ];
 
